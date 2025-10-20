2655524a5eba252a05ea51c1a1151ea684f3a073
Pavel S <sprrps@icloud.com>
2025-10-20 22:02:02 +0300

    3 lesson

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 new WOW().init();
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    bulletClass: "project-bullet",
+    bulletActiveClass: "project-bullet-active",
+
+    clickable: true,
+  },
+});
